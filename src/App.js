2e1ceca91ffcdb7e39dbeb8f3d67eb5041b782f0
Aayush <aayushsunar87@gmail.com>
2022-09-27 19:49:52 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import './style.css';
+const App = () => {
+    const [number,setNumber]=useState(0)
+    return <div>
+        <h1>TIME COUNTER</h1>
+     <p>{number}</p>
+        <button onClick={()=>setNumber(number-1)} className="button">-</button>
+        <button onClick={()=>setNumber(0)} className="button">RESET</button>
+        <button onClick={()=>setNumber(number+1)} className="button">+</button>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
-  );
-}
 
+}
 export default App;
